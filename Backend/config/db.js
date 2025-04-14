9e59d56d9d1d676387869eb051b323ace5b848f1
DiegoSoriaGalvarro <109701935+DiegoSoriaGalvarro@users.noreply.github.com>
2025-04-14 18:56:05 -0300

    Update db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -7,10 +7,7 @@ dotenv.config(); // Cargar variables de entorno
 
 const connectDB = async () => {
   try {
-    const connection = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const connection = await mongoose.connect(process.env.MONGO_URI);
 
     console.log(`✅ MongoDB conectado: ${connection.connection.host}`);
   } catch (error) {
